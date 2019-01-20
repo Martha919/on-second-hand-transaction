be91021ad2ee60dc741223c30cbd2d62cd4e1474
Martha919 <duran@pku.deu.cn>
2019-01-20 21:40:40 +0800

    first commit

---
+++ pages/add/add.js
@@ -0,0 +1,167 @@
+// pages/add/add.js
+var util = require('../../utils/util.js');
+var app = getApp();
+
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {//定义data初始值
+    img: null,
+    classId: null,
+    classification: null,
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+  */
+  onShow: function () {
+    var that = this
+    wx.request({//调用wx.request()，
+      url: 'https://www.parcruz.site/api/face' + '/goods/class',//请求地址
+      success: function (res) {
+        that.setData({//调用setData，修改this.data的值
+          classification: res.data.data
+        })
+      }
+    })
+  },
+
+
+
+  //从后台获取数据
+  updataimg: function () {//定义函数名称
+    var that = this
+    wx.chooseImage({
+      sourceType: ['album', 'camera'],
+      success: function (res) {
+        //
+        var tempFilePaths = res.tempFilePaths
+        that.setData({
+          img: tempFilePaths
+        })
+        console.log(res.tempFiles)
+        // console.log("临时路径"+tempFilePaths)
+      }
+    })
+  },
+  formSubmit: function (e) {
+    // console.log(e.detail.value)
+    var that = this
+    var value = e.detail.value
+    if (value.name == "") {
+      wx.showToast({
+        title: '商品标题不能为空',
+        icon: "none"
+      })
+    } else if (value.describle == "") {
+      wx.showToast({
+        title: '商品描述不能为空',
+        icon: "none"
+      })
+    } else if (value.price < 0 || value.price == "" || value.price > 99999999) {
+      wx.showToast({
+        title: '价格不能小于零',
+        icon: "none"
+      })
+    } else if (that.data.img == null) {
+      wx.showToast({
+        title: '你最好能添加几张图片',
+        icon: "none"
+      })
+    } else if (that.data.classId == null) {
+      wx.showToast({
+        title: '你是不是没选分类啊',
+        icon: "none"
+      })
+    } else {
+      //这里添加一个延迟 以免用户多次添加
+      wx.showToast({
+        title: '发布中',
+        icon: 'loading',
+        duration: 30000
+      })
+      console.log("userId=" + app.data.userInfo.id +
+        " classificationId=" + that.data.classId +
+        " describle=" + value.describle +
+        " name=" + value.name,
+        " price=" + value.price)
+      wx.request({
+        url: www.parcruz.site/api/face + '/goods/add',
+        method: 'POST',
+        header: {
+          'content-type': 'application/x-www-form-urlencoded'
+        },
+        data: {
+          userId: app.data.userInfo.id,
+          classificationId: that.data.classId,
+          commetNum: 0,
+          describle: value.describle,
+          name: value.name,
+          price: value.price,
+          status: 0,
+          pageView: 0,
+          updateTime: util.formatTime(new Date())
+        },
+        success: function (res) {
+          console.log(res.data)
+          if (that.data.img != null) {
+            for (var i = 0; i < that.data.img.length; i++) {
+              wx.showToast({
+                icon: 'loading',
+                title: '正在上传'
+              })
+              console.log(that.data.img[i])
+              wx.uploadFile({
+                url: app.data.apiUrl + '/img/add',
+                filePath: that.data.img[i],
+                name: 'file',
+                formData: {
+                  goodsId: res.data.data.id,
+                },
+                success: function (res) {
+                  console.log(res.data)
+                  wx.showToast({
+                    title: '上传成功',
+                  })
+                  wx.switchTab({
+                    url: '/pages/index/index',
+                  })
+                },
+                fail: function () {
+                  wx.showToast({
+                    icon: 'none',
+                    title: '上传失败',
+                  })
+                }
+              })
+            }
+          }
+        }
+      })
+    }
+  },
+  bindPickerChange: function (e) {
+    var that = this
+    console.log('携带值为', e.detail.value)
+    that.setData({
+      classId: that.data.classification[e.detail.value].id
+    })
+  },
+})
---
+++ pages/my/my.js
@@ -0,0 +1,95 @@
+var app = getApp();
+
+Page({
+  data: {
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    // userInfo:null,
+    //判断用户是否授权 来决定是否显示登录按钮
+    isUser: false,
+    userInfoForSql:null
+  },
+
+  logBtn:function(options){
+    //跳转到登录界面
+    wx.navigateTo({
+      url:'../login/login'
+    })
+  },
+
+ 
+ onLoad:function(){
+   var that = this
+   if(that.data.userInfoForSql==null){
+     wx.request({
+       url: 'https://www.parcruz.site/api/face' + '/user/openid/' + app.data.openid,
+       method: 'GET',
+       success:function(res){
+         if(res.data.data!=null){
+           that.setData({
+             userInfoForSql: res.data.data,
+             isUser: true,
+           })
+         }
+         console.log("my页 26行")
+         console.log(res.data.data)
+         app.data.userInfo = res.data.data
+       }
+     })
+   }
+ },
+
+  bindGetUserInfo: function (e) {
+    var that = this 
+    console.log(e.detail.userInfo)
+    var userInfo = e.detail.userInfo
+    if(app.data.openid!=null){
+      //添加一个延迟 以免有人手速太快
+      wx.showToast({
+        title: '',
+        icon: 'loading',
+        duration: 1000
+      })
+      wx.request({
+        //检测此openid是否注册
+        url: 'https://www.parcruz.site/api/face' + '/user/openid/' + app.data.openid,
+        method: 'GET',
+        success: function (res) {
+          //假如此账号未注册
+          if (res.data.data == null) {
+            wx.request({
+              url: app.data.apiUrl + '/user/',
+              method: 'POST',
+              header: {
+                'content-type': 'application/x-www-form-urlencoded'
+              },
+              data: {
+                username: userInfo.nickName,
+                password: '',
+                qq: '',
+                email: '',
+                goodsNum: 0,
+                power: 0,
+                status: 0,
+                userImg: userInfo.avatarUrl,
+                phone: '',
+                openid: app.data.openid
+              },
+              success: function (res) {
+                console.log("post后返回的数据" + res.data.msg + res.data.data.username)
+              }
+            })
+          }else{
+            //账号已注册 将信息传入userInfoSql
+            that.setData({
+              userInfoForSql:res.data.data,
+              isUser: true,
+            })
+            //同时把数据也传递给全局变量userInfo
+            app.data.userInfo = res.data.data
+            console.log(that.data.userInfoForSql)
+          }
+        }
+      })
+    }
+  },
+})
